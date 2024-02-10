123eaa137cc09c6365aeac7dedace09de2886797
keerthanasriniv <117429428+keerthanasriniv@users.noreply.github.com>
2024-02-10 09:57:20 +0530

    Add files via upload

---
+++ backend/Day8/model/Event.java
@@ -0,0 +1,63 @@
+package com.learn.springsecurity.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "event")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String image;
+    private int price;
+    private String description;
+
+    // Constructors
+    public Event() {
+    }
+
+    public Event(String image, int price, String description) {
+        this.image = image;
+        this.price = price;
+        this.description = description;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
